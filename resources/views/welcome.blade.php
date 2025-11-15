2879f632194692ec8c9e0bdc03efd25172e6581a
NDAK GAMERS <devinip2000@gmail.com>
2025-11-15 13:32:44 +0700

    nambahin tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
     <title>CampRent</title>
 </head>
-<body>
-    <h1>Hello World!</h1>
+<body class="bg-black">
+    <h1 class="text-white">Hello World!</h1>
 </body>
 </html>
